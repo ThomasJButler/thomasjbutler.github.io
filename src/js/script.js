274901cfbf18cd0be0522e969ae909b546d24eba
tommyprompts <thomasjbutler25@gmail.com>
2024-06-06 00:31:55 +0100

    v1.1 - Website
    v1.1 with new and improved website design

---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Welcome to Tom's Development Journey!");
+    // Feel free to expand this script with more interactive elements!
+});
